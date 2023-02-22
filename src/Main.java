6887c48182b6c47910f4d7f09230da9fef464702
KaaL-EL <Professor_ParadoxGz1@protonmail.ch>
2023-02-23 00:08:26 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //Main method for running the application.
+    public static void main(String[] args) {
+        Frames frame = new Frames();
+        frame.openFrame();
+    }
+}
