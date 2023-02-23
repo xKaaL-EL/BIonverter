ff20bafdb08fa621ec9a542aae2206f0e871ab7f
KaaL-EL <Professor_ParadoxGz1@protonmail.ch>
2023-02-23 15:23:55 +0530

    Delaying The Conversion

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    //Main method for running the application.
-    public static void main(String[] args) {
-        Frames frame = new Frames();
-        frame.openFrame();
-    }
-}
